d77bab993c46d26b162c34203b6d912376a93e73
atifar <tyatya68@gmail.com>
2017-04-09 20:16:15 -0700

    replace missed TEST_EMAIL strings

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -77,7 +77,7 @@ class LoginTest(FunctionalTest):
         # she is logged in!
         self.wait_to_be_logged_in(email=test_email)
         navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.assertIn(test_email, navbar.text)
 
         # Now she logs out
         self.browser.find_element_by_link_text('Log out').click()
@@ -85,4 +85,4 @@ class LoginTest(FunctionalTest):
         # She is logged out
         self.wait_to_be_logged_out(email=test_email)
         navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.assertNotIn(test_email, navbar.text)
